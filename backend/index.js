eaa6e06c920d20e4d573d7ffcb457cac99c9a27b
mxmkrg <maximka200024@gmail.com>
2025-06-01 18:41:07 +0200

    Moved all frontend files to frontend folder

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,29 @@
 const express = require('express');
-const app = express();
-
+const dotenv = require('dotenv');
+const cors = require('cors');
 const statusRouter = require('./routes/status');
 const usersRouter = require('./routes/users');
 
+// Load environmet variables
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use('/api/status', statusRouter);
 app.use('/api/users', usersRouter);
 
-const PORT = 3000;
+// Standard route
+app.get('/', (req, res) => {
+  res.send('TabRes ist aktiv');
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server läuft auf http://localhost:${PORT}`);
 });
